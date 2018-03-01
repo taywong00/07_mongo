26092adcb6efc4e3f9f3b4b14fa974e8d1765dae
Taylor Wong <Taylor Wong>
2018-03-01 10:46:35 -0500

    yuh

--- connect.js
+++ connect.js
@@ -1,3 +1,6 @@
+var lastX;
+var lastY;
+
 var pic = document.getElementById('vimage');
 
 var drawDot = function(e) {
@@ -16,6 +19,37 @@ var drawDot = function(e) {
 
 }
 
-var connect = function
+var connect = function(e) {
 
-pic.addEventListener('click', drawDot);
+  if (lastX) {
+
+    console.log('there is a lastX');
+    var line = document.createElementNS(
+      'http://www.w3.org/2000/svg',
+      'line'
+    );
+
+    line.setAttribute('x1', e.offsetX);
+    line.setAttribute('y1', e.offsetY);
+    line.setAttribute('x2', lastX);
+    line.setAttribute('y2', lastY);
+    line.setAttribute('stroke', 'blue');
+    line.setAttribute('stroke-width', '1');
+
+    pic.appendChild(line);
+  }
+
+  lastX = e.offsetX;
+  lastY = e.offsetY;
+  console.log('NEW LASTX: ' + lastX);
+  console.log('NEW LASTY: ' + lastY);
+
+}
+
+var draw = function(e) {
+  drawDot(e);
+  connect(e);
+  console.log('drawing!');
+}
+
+pic.addEventListener('click', draw);
